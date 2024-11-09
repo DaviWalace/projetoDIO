d39ef76a42c99ad8fca210a0c4505dc1c3d69120
DaviWalace <walace10davi@gmail.com>
2024-11-09 19:45:11 -0300

    mensagem de commit

--- calculadoraDeRanque.js
+++ calculadoraDeRanque.js
@@ -1,26 +1,27 @@
-var vitorias = 10
-var derrotas = 10
-var resultado
-function ranque(vitorias, derrotas){
+var vitorias = 150
+var derrotas = 20
+var soma = (vitorias - derrotas)
+function resultado(vitorias, derrotas){
     return (vitorias - derrotas)}
-    resultado = ranque(150, 20)
-if (resultado <= 10) { resultado = "Ferro"}
-else if (resultado >= 11 && resultado <= 20) {
-    resultado = "Bronze"}
-else if (resultado >= 21 && resultado <= 50) {
-    resultado = "Prata"}
-else if (resultado >= 51 && resultado <= 70) {
-    resultado = "Ouro"}
-else if (resultado >= 71 && resultado <= 80) {
-    resultado = "Platina"}
-else if (resultado >= 81 && resultado <= 90) {
-    resultado = "Ascendente"}
-else if (resultado >= 91 && resultado <= 100) {
-    resultado = "Imortal"}
-else if (resultado >= 101) {
-    resultado = "Radiante"}
-
+    var ranque = resultado(vitorias, derrotas)
+    while (ranque >= 10){
+if (ranque <= 10) { ranque = "Ferro"}
+else if (ranque >= 11 && ranque <= 20) {
+    ranque = "Bronze"}
+else if (ranque >= 21 && ranque <= 50) {
+    ranque = "Prata"}
+else if (ranque >= 51 && ranque <= 70) {
+    ranque = "Ouro"}
+else if (ranque >= 71 && ranque <= 80) {
+    ranque = "Platina"}
+else if (ranque >= 81 && ranque <= 90) {
+    ranque = "Ascendente"}
+else if (ranque >= 91 && ranque <= 100) {
+    ranque = "Imortal"}
+else if (ranque >= 101) {
+    ranque = "Radiante"}
+    }
    
     
 
-console.log(resultado)
+console.log("O Herói tem de saldo de: "+ soma + " e está no nível de: "+ ranque)
