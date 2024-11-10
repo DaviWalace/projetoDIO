b271ddf8008770a0d4af2ebf564af715a527b561
DaviWalace <walace10davi@gmail.com>
2024-11-10 19:59:15 -0300

    mensagem de commit

--- classesDoGame.js
+++ classesDoGame.js
@@ -1,27 +1,24 @@
-var tipo = 'Cavaleiro'
+const tipo = ["Mago","Guerreiro","Monge","Ninja"]
+var classeEscolhida = tipo[2]
 class personagem{
     constructor(nome,idade,classe){
     this.nome = "Davi"
     this.idade = "21"
-    this.classe = tipo
+    this.classe = classeEscolhida
     }    
     atacar() {
        
         if(this.classe === 'Mago')
             {this.ataque = 'magia'}
-        else if (this.classe === 'Cavaleiro') 
+        else if (this.classe === 'Guerreiro') 
             {this.ataque = 'espada'} 
         else if (this.classe === 'Monge') 
             {this.ataque = 'artes marciais'} 
         else if (this.classe === 'Ninja') 
             {this.ataque = 'shuriken'}
-          }
-        
-    escrever(){
         console.log(`O ${this.classe} atacou usando ${this.ataque}`)
+          }  
 }
-}
-var dadosPersonagem = new personagem
-dadosPersonagem.atacar()
-dadosPersonagem.escrever();
 
+var dadosPersonagem = new personagem ('Davi', "21", classeEscolhida)
+dadosPersonagem.atacar()
